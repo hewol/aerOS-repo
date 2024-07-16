bf9f656f2dabfe59f16c762c100d5a9cd19589da
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-07-16 12:53:52 +0000

    we do not want to remove non-NG packages

--- update_db.sh
+++ update_db.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 if command -v repo-add; then
-  for arch in x86_64; do
-    ( cd $arch && rm -rf ./*.tar.gz && repo-add --remove packages-aeros.db.tar.gz ./*.pkg.tar.zst )
-  done
+    ( cd x86_64 && rm -rf ./*.tar.gz && repo-add packages-aeros.db.tar.gz ./*.pkg.tar.zst )
 else
   printf "\e[0;31m==> ERROR:\e[0m\`repo-add\` wasn't found, you might not be running Arch Linux.\n"
   exit 1
